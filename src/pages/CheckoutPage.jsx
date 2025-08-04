7270a3a8686f42d0364df7e4d4b8d58b1f601425
parham khoram <khoramparham@gmail.com>
2025-08-04 18:19:03 +0330

    complete basket

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import { useCart } from "../context/CartContext";
+import BasketCard from "../components/BasketCard";
 
 function CheckoutPage() {
+  const [state, dispatch] = useCart();
+  const clickHandler = (type, payload) => dispatch({ type, payload });
   return (
-    <div>CheckoutPage</div>
-  )
+    <div>
+      <div>
+        {state.selectedItems.map((product) => (
+          <BasketCard key={product.id} data={product} clickHandler={clickHandler} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default CheckoutPage
+export default CheckoutPage;
