a13f0a6dc626e99748dc8ecbb382424ea85de1d0
parham khoram <khoramparham@gmail.com>
2025-08-04 18:48:40 +0330

    search box styled-checkout fixed

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -1,13 +1,22 @@
 import React from "react";
 import { useCart } from "../context/CartContext";
 import BasketCard from "../components/BasketCard";
-
+import BasketSideBar from "../components/BasketSideBar";
+import styles from "./Checkout.module.css";
 function CheckoutPage() {
   const [state, dispatch] = useCart();
+  if (!state.itemsCounter) {
+    return (
+      <div>
+        <p>Empty</p>
+      </div>
+    );
+  }
   const clickHandler = (type, payload) => dispatch({ type, payload });
   return (
-    <div>
-      <div>
+    <div className={styles.container}>
+      <BasketSideBar state={state} clickHandler={clickHandler} />
+      <div className={styles.products}>
         {state.selectedItems.map((product) => (
           <BasketCard key={product.id} data={product} clickHandler={clickHandler} />
         ))}
